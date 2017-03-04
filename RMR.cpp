bd66ad2b69d29d040a95f06d03a724a7152ec65f
Irfanamean <tanimamin96@gmail.com>
2017-03-04 14:24:32 +0600

    Add files via upload

---
+++ RMR.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+int x;
+char y;
+float z;
+cout<<"enter ur nember"<<endl;
+cin>>x;
+cin>>y;
+cin>>z;
+z=x+y;
+cout<<"z"
+
+
+
+return 0;
+
+}
